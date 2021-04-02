1f43aae4a0b6782a0a8341e4b91df6ff75e23714
Rodrigo Souza <rodrigaosouza27@hotmail.com>
2021-04-02 00:27:36 -0300

    Update create-point.js

--- js/create-point.js
+++ js/create-point.js
@@ -1,4 +1,37 @@
-document.querySelector("select[name=uf]")
-.addEventListener("change", () => {
-    console.log('mudei')
-} )
+function populateUFs() {
+    const ufSelect = document.querySelector("select[name=uf]")
+
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    .then(res => res.json())
+    .then(states => {
+
+        for( const state of state ) {
+
+            ufSelect.innerHTML += `<option value="1">Valor</option>`
+        }
+
+        
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//document.querySelector("select[name=uf]")
+//.addEventListener("change", () => {
+//    console.log('mudei')
+//} )
