4ba22789b6cdc126d6d3977a31fcd902a361fe05
Rodrigo Souza <rodrigaosouza27@hotmail.com>
2021-04-02 01:07:54 -0300

    Update create-point.js

--- js/create-point.js
+++ js/create-point.js
@@ -5,12 +5,11 @@ function populateUFs() {
     .then(res => res.json())
     .then(states => {
 
-        for( const states of states ) {
+        for(  state of states ) {
 
-            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+            ufSelect.innerHTML += ` <option value="${state.id}">${state.nome}</option> `
         }
 
-        
     })
 }
 
@@ -18,12 +17,29 @@ function populateUFs() {
 populateUFs()
 
 
+function getCities(event) {
+    const citySelect = document.querySelector("select[name=city]")
 
+    const ufValue = event.target.value
+
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+    fetch(url)
+    .then(res => res.json())
+    .then(cities => {
+
+        for(  city of cities ) {
+
+            citySelect.innerHTML += ` <option value="${city.id}">${city.nome}</option> `
+        }
+
+        citySelect.disabled = false
+
+    })
+}
 
 
 
 
 document.querySelector("select[name=uf]")
-.addEventListener("change", () => {
-    console.log('mudei')
-} )
+.addEventListener("change", getCities)
