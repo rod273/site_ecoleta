4ac0147bb096241f1a5792569edd2e9ca3d20277
Rodrigo Souza <rodrigaosouza27@hotmail.com>
2021-04-03 16:15:48 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const server = express();
 
+
+//CONFIGURAR CAMINHOS DA MINHA APLICAÇÃO
+//PAGINA INICIAL
+//REQ : REQUISIÇÃO
+// RES: RESPOSTA
+server.get("/", (req, res) => {
+
+})
+
 //LIGAR O SERVIDOR
 server.listen(3000)
