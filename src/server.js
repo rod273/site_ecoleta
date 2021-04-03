9544dd41578ede4dc9b163c4f8c60495c5dd4072
Rodrigo Souza <rodrigaosouza27@hotmail.com>
2021-04-03 16:26:54 -0300

    nodemon

--- src/server.js
+++ src/server.js
@@ -2,12 +2,16 @@ const express = require('express');
 const server = express();
 
 
+//CONFIGURAR PASTA PÚBLIC
+server.use(express.static("public"))
+
+
 //CONFIGURAR CAMINHOS DA MINHA APLICAÇÃO
 //PAGINA INICIAL
 //REQ : REQUISIÇÃO
 // RES: RESPOSTA
 server.get("/", (req, res) => {
-
+   res.sendFile(__dirname + "/views/index.html")
 })
 
 //LIGAR O SERVIDOR
