369cc05692c02e4485a7ceadf30037efb8f0c87e
Rodrigo Souza <rodrigaosouza27@hotmail.com>
2021-04-04 10:51:13 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
-const express = require('express');
-const server = express();
+const express = require('express')
+const server = express()
 
 //PEGAR O BANCO DE DADOS
- const db = require("./database/db.js");
+ const db = require("./database/db.js")
 
 //CONFIGURAR PASTA PÚBLIC
-server.use(express.static("public"));
+server.use(express.static("public"))
+
+// HABILITAR O USO DO req.body NA NOSSA APLICAÇÃO
+server.use(express.urlencoded({extended: true}))
 
 
 //URILIZANDO TEMPLATE ENGINE
@@ -29,11 +32,70 @@ server.get("/", (req, res) => {
 server.get("/create-point", (req, res) => {
 
    //req.query: Query strings da nossa url
-    console.log(req.query) 
+   // console.log(req.query) 
 
 
    return res.render("create-point.html")
- });
+ })
+
+
+ server.post("/savepoint", (req, res) => {
+
+  // req.body: O corpo do nosso formulario
+  // console.log(req.body)
+
+  //INSERIR DADOS NO BANCO DE DADOS
+          const query = `
+                INSERT INTO places (
+                    image,
+                    name,
+                    address,
+                    address2,
+                    state,
+                    city,
+                    items
+                ) VALUES (?,?,?,?,?,?,?);
+        `
+        const values = [
+
+          req.body.image,
+          req.body.name,
+          req.body.address,
+          req.body.address2,
+          req.body.state,
+          req.body.city,
+          req.body.items
+
+ 
+      ]
+
+        function afterInsertData(error) {
+           if(error) {
+               return console.log(error)
+           }
+
+           console.log("Cadastrado com Sucesso")
+            console.log(this)
+
+            return res.render("create-point-html", {saved: true})
+        }
+
+
+     db.run(query, values, afterInsertData)
+
+      //  Consultar os dados da tabela 
+       db.all(` SELECT name FROM places`, function(error, rows) {
+        if(error) {
+            return console.log(error)
+       }
+
+       console.log("Aqui estão os seus Registros :")
+       console.log(rows)
+    })  
+
+
+  
+ })
 
 
  server.get("/search", (req, res) => {
